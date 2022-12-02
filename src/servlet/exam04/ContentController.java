46c83c114e53cc1a057f7f6c520d42e36a50f431
LemonGlacier <hijy02122@gmail.com>
2022-12-02 19:13:50 +0900

    20221202 servlet jsp contextPath 동적 수정

--- src/servlet/exam04/ContentController.java
+++ src/servlet/exam04/ContentController.java
@@ -19,7 +19,7 @@ public class ContentController extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("exam04.ContentController.doPost() 실행");
-		response.sendRedirect("/servletjsp/HomeController");
+		response.sendRedirect(request.getContextPath() + "/HomeController");
 		//request.getRequestDispatcher("/WEB-INF/views/exam04/content.jsp").forward(request, response);
 	}
 	
